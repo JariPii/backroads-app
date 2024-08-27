79db0aa137703ae9a5b1daf63eae92e2d2c0a4a2
Jari Leminaho <jaripiilola@gmail.com>
2024-08-27 14:30:53 +0200

    Initial

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import { pageLinks, socialLinks } from '../data';
+import PageLinks from './PageLinks';
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      {/* <ul className="footer-links">
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+
+      <ul className="footer-icons">
+        {socialLinks.map((social) => {
+          const { id, href, icon } = social;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"> {new Date().getFullYear()} </span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
